07cec68c8a4e8f370cd43a829ae22d7eb0b46fd2
yunxiangyong <yunxiangyong@zuoyebang.com>
2021-09-01 16:33:44 +0800

    初始化代码

---
+++ app/src/main/java/com/yxy/mydesign/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yxy.mydesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yxy.structure.core.AppSpec;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
